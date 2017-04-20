5dcc3d0803e2e826ff301a8080e5f02478d8a1f4
thachnhoang <hoangngocthachbk@gmail.com>
2017-04-19 20:34:46 -0500

    Add files via upload

--- SuperChat.cpp
+++ SuperChat.cpp
@@ -288,12 +288,20 @@ message_data Message ( (char*) "msg" );
 message_list_t  message_List;
 chatroom_list_t  room_List;
 user_list_t  user_List;
+int sig;
+char str_user[8];
+int open_window2 = 0;//use to check if it's the first time to open window or not
+int open_window3 = 0;
+int show_msg1 = 1; //show message for window 2
+int show_msg2 = 22;
 
 void window1(unsigned int &);
 void window2(unsigned int);
+void window3(unsigned int);
 void *show_message(void *ptr);
 void *get_message(void *t);
-
+void *show_message1(void *ptr);
+void *get_message1(void *t);
 
 int main()
 {	
@@ -304,11 +312,22 @@ int main()
    //create new user
    
    unsigned int uuid;
-   window1(uuid);
-   	
+   window1(uuid);  
+
+   while(1)
+   {
+	sig = 0;
+	if(sig == 0)
+	{
+	   window2(uuid);	   
+	}
+
+	if(sig == 1)
+	{
+	   window3(uuid);
+	}
+   }
    
-   window2(uuid);
-	
    endwin();
    return 0;
 }
@@ -317,8 +336,7 @@ void window1(unsigned int &uuid)
 {
 	user user_info;
 	user_info.chatroom_idx = 1;
-	char str_user[8];
-
+	
 	WINDOW *my_wins[3];
 	PANEL  *my_panels[3];
 	int lines = 25, cols = 110, y = 2, x = 4, i;
@@ -385,68 +403,76 @@ void window1(unsigned int &uuid)
 
 void window2(unsigned int uuid)
 {	
-	WINDOW *my_wins[4];
-	PANEL  *my_panels[4];
 	//int lines = 25, cols = 110, y = 1, x = 4, i;
 	int i;
 	char str_msg[144];
 	
+	if(open_window2 == 0)
+	{
+		WINDOW *my_wins[4];
+		PANEL  *my_panels[4];
+		initscr();
+		cbreak();
+		echo();
+		start_color(); // type this first in order to use color;
 
-	initscr();
-	cbreak();
-	echo();
-	start_color(); // type this first in order to use color;
+		// Initialize all the colors
+		init_pair(1, COLOR_YELLOW, COLOR_BLACK);
 
-	// Initialize all the colors
-	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
-
-	// Create windows for the panels
-	//              (lines, cols, y, x);
-	//my_wins[0] = newwin(34, 109, y, x); // layout window
-	my_wins[0] = newwin(20, 70, 0 ,0); // public chat window
-	my_wins[1] = newwin(3, 48, 16, 1); // enter text window
-	my_wins[2] = newwin(9, 19.5, 1, 50); // chat room window
-	my_wins[3] = newwin(9, 19.5, 10, 50); // user window
+		// Create windows for the panels
+		//              (lines, cols, y, x);
+	
+		my_wins[0] = newwin(20, 70, 0 ,0); // public chat window
+		my_wins[1] = newwin(3, 48, 16, 1); // enter text window
+		my_wins[2] = newwin(9, 19.5, 1, 50); // chat room window
+		my_wins[3] = newwin(9, 19.5, 10, 50); // user window
 	
 
-	 
-	// Create borders around the windows so that you can see the effect
-	//  of panels
-	 
-	for(i = 0; i < 4; ++i) {
-		wattron(my_wins[i],COLOR_PAIR(1)); //turn on window border color = yellow
-		box(my_wins[i], 0, 0);
-		wattroff(my_wins[i],COLOR_PAIR(1)); // turn off color
+		 
+		// Create borders around the windows so that you can see the effect
+		//  of panels
+		 
+		for(i = 0; i < 4; ++i) {
+			wattron(my_wins[i],COLOR_PAIR(1)); //turn on window border color = yellow
+			box(my_wins[i], 0, 0);
+			wattroff(my_wins[i],COLOR_PAIR(1)); // turn off color
+		}
+
+		// Attach a panel to each window 
+	
+		my_panels[0] = new_panel(my_wins[0]); // chat window
+		my_panels[1] = new_panel(my_wins[1]); // enter text window
+		my_panels[2] = new_panel(my_wins[2]); // chat room window
+		my_panels[3] = new_panel(my_wins[3]); // user window
+
+		// Update the stacking order. 2nd panel will be on top
+		update_panels();
+
+		// Show it on the screen
+		doupdate();
+
+		mvprintw(2, 55, "Chat Room"); // print text "Chat Room" at this location
+		mvprintw(11, 57, "Users"); // print text "Users" at this location
+
+		// Enter puclic chat text here at this location inside panel[2]
+		mvprintw(17, 2, "Msg>");
+
+		//create chatroom
+	   	chatroom chatroom_info;
+	   	chatroom_info.chatroom_idx = 1;
+	   	strcpy(chatroom_info.chatroom_name, "Room1");
+	   	chatRoom.send(chatroom_info);
+		chatRoom.recv ( &room_List );	
+		mvprintw(4, 52, chatroom_info.chatroom_name);
+
+		open_window2++;
 	}
-
-	// Attach a panel to each window 
-
-	//my_panels[0] = new_panel(my_wins[0]); // layout window
 	
-	my_panels[0] = new_panel(my_wins[0]); // chat window
-	my_panels[1] = new_panel(my_wins[1]); // enter text window
-	my_panels[2] = new_panel(my_wins[2]); // chat room window
-	my_panels[3] = new_panel(my_wins[3]); // user window
-
-	// Update the stacking order. 2nd panel will be on top
-	update_panels();
-
-	// Show it on the screen
-	doupdate();
-
 	mvprintw(2, 55, "Chat Room"); // print text "Chat Room" at this location
 	mvprintw(11, 57, "Users"); // print text "Users" at this location
 
 	// Enter puclic chat text here at this location inside panel[2]
 	mvprintw(17, 2, "Msg>");
-
-	//create chatroom
-   	chatroom chatroom_info;
-   	chatroom_info.chatroom_idx = 1;
-   	strcpy(chatroom_info.chatroom_name, "Room1");
-   	chatRoom.send(chatroom_info);
-	chatRoom.recv ( &room_List );	
-	mvprintw(4, 52, chatroom_info.chatroom_name);
 
 	pthread_t thread1, thread2;
 	//pthead to print out chat message
@@ -473,12 +499,121 @@ void window2(unsigned int uuid)
 	{
 	  perror("Problem with pthread_join: ");
 	}
-	refresh();
-	getch();
-	endwin();
+	//refresh();
+	//getch();
+	//endwin();
 	
 }
 
+void window3(unsigned int uuid)
+{	
+	int i;
+	char str_msg[144];
+	
+	if(open_window3 == 0)
+	{
+		WINDOW *my_wins[4];
+		PANEL  *my_panels[4];
+		initscr();
+		cbreak();
+		echo();
+		start_color(); // type this first in order to use color;
+
+		// Initialize all the colors
+		init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+
+		// Create windows for the panels
+		//              (lines, cols, y, x);
+	
+		my_wins[0] = newwin(18, 70, 21 ,0); // public chat window
+		my_wins[1] = newwin(3, 48, 35, 1); // enter text window
+		my_wins[2] = newwin(7, 19.5, 22, 50); // chat room window
+		my_wins[3] = newwin(9, 19.5, 29, 50); // user window
+	
+
+		 
+		// Create borders around the windows so that you can see the effect
+		//  of panels
+		 
+		for(i = 0; i < 4; ++i) {
+			wattron(my_wins[i],COLOR_PAIR(1)); //turn on window border color = yellow
+			box(my_wins[i], 0, 0);
+			wattroff(my_wins[i],COLOR_PAIR(1)); // turn off color
+		}
+
+		// Attach a panel to each window 
+	
+		my_panels[0] = new_panel(my_wins[0]); // chat window
+		my_panels[1] = new_panel(my_wins[1]); // enter text window
+		my_panels[2] = new_panel(my_wins[2]); // chat room window
+		my_panels[3] = new_panel(my_wins[3]); // user window
+
+		// Update the stacking order. 2nd panel will be on top
+		update_panels();
+
+		// Show it on the screen
+		doupdate();
+		
+		mvprintw(23, 55, "Chat Room"); // print text "Chat Room" at this location
+		mvprintw(30, 57, "Users"); // print text "Users" at this location
+
+		// Enter puclic chat text here at this location inside panel[2]
+		mvprintw(36, 2, "Msg>");
+		
+		//create chatroom
+	   	chatroom chatroom_info;
+	   	chatroom_info.chatroom_idx = 2;
+	   	strcpy(chatroom_info.chatroom_name, "Room2");
+	   	chatRoom.send(chatroom_info);
+		chatRoom.recv ( &room_List );	
+		mvprintw(25, 52, chatroom_info.chatroom_name);
+
+		//create user
+		user user_info;
+		user_info.chatroom_idx = 2;
+		user_info.uuid = uuid;
+		strncpy(user_info.nick, str_user, sizeof(user_info.nick));
+		User.send(user_info);
+
+		open_window3++;
+	}
+	
+	mvprintw(23, 55, "Chat Room"); // print text "Chat Room" at this location
+	mvprintw(30, 57, "Users"); // print text "Users" at this location
+
+	// Enter puclic chat text here at this location inside panel[2]
+	mvprintw(36, 2, "Msg>");
+		
+	pthread_t thread3, thread4;
+	//pthead to print out chat message
+	if(pthread_create(&thread3, NULL, show_message1, NULL))
+	{
+	  perror("Error creating thread3: ");
+	  exit(EXIT_FAILURE);
+	}
+	
+	//pthead to send message to database
+	unsigned long t = uuid;
+	if(pthread_create(&thread4, NULL, get_message1,(void *) t))
+	{
+	  perror("Error creating thread4: ");
+	  exit(EXIT_FAILURE);
+	}
+	
+	if(pthread_join(thread3, NULL))
+	{
+	  perror("Problem with pthread_join: ");
+	}
+	
+	if(pthread_join(thread4, NULL))
+	{
+	  perror("Problem with pthread_join: ");
+	}
+	//refresh();
+	//getch();
+	//endwin();
+	
+}
 
 void *get_message(void *t)
 {
@@ -487,6 +622,15 @@ void *get_message(void *t)
   while(1)
   {   	
 	mvgetstr(17,6, str_msg);
+
+	if(strcmp(str_msg, "/q") == 0 )
+	{
+	   sig = 1;
+	   mvprintw(17,6, "                                         ");
+	   mvprintw(36,6, "");
+	   break;
+	}
+
 	if(strncmp(str_msg, "/room:", 6) == 0)
 	{
 	   chatroom chatroom_info;
@@ -519,12 +663,63 @@ void *get_message(void *t)
   }
 }
 
+void *get_message1(void *t)
+{
+  char str_msg[144];
+
+  while(1)
+  {   	
+	mvgetstr(36,6, str_msg);
+
+	if(strcmp(str_msg, "/q") == 0 )
+	{
+	   sig = 0;
+	   mvprintw(36,6, "                                         ");
+	   mvprintw(17,6, "");
+	   break;
+	}
+
+	if(strncmp(str_msg, "/room:", 6) == 0)
+	{
+	   chatroom chatroom_info;
+   	   chatroom_info.chatroom_idx = 2;
+   	   strncpy(chatroom_info.chatroom_name, str_msg + 6, sizeof(chatroom_info.chatroom_name));
+   	   chatRoom.send(chatroom_info);
+	   mvprintw(36,6, "                                         ");
+	   
+	   continue;  
+	}
+
+	if(strncmp(str_msg, "/name:", 6) == 0)
+	{
+	   user user_info;
+	   user_info.chatroom_idx = 2;
+	   user_info.uuid = (unsigned long) t; 
+	   strncpy(user_info.nick, str_msg + 6, sizeof(user_info.nick)); 
+	   User.send(user_info);
+	   mvprintw(36,6, "                                         ");
+	   
+	   continue; 
+	}
+
+	message message_info;	
+	message_info.cksum = 0;
+	message_info.uuid = (unsigned long) t; 
+	message_info.chatroom_idx = 2;		
+	strncpy(message_info.message, str_msg, sizeof(message_info.message));
+	mvprintw(36,6, "                                         ");
+	
+	Message.send(message_info);
+	
+  }
+}
+
 void *show_message(void *ptr)
 {
   int count_message = 0;
   int count_user = 0;
   int count_room = 0;
-  int show_msg = 1;
+  
   int second_run = 0;
   int duplicate_user;
 
@@ -534,11 +729,15 @@ void *show_message(void *ptr)
     	Message.recv ( &message_List );
     	User.recv (&user_List);
 	chatRoom.recv(&room_List);
-
-	//clear out line when chat message is a lot
-	if(show_msg == 15)
+	
+	if(sig == 1)
 	{
-		show_msg = 1;
+	   break;
+	}
+	//clear out line when chat message is a lot
+	if(show_msg1 == 15)
+	{
+		show_msg1 = 1;
 		int new_show_msg;
 		for(new_show_msg = 1; new_show_msg < 16; new_show_msg++)
 		{
@@ -570,20 +769,20 @@ void *show_message(void *ptr)
 	     duplicate_user = 0;
 	     for(int j = i + 1; j < user_List.size(); j++ )
 	     {
-		if(user_List[i].uuid == user_List[j].uuid)
+		if(user_List[i].uuid == user_List[j].uuid && user_List[i].chatroom_idx == 1 && user_List[j].chatroom_idx == 1)
 		{
 		   duplicate_user = 1;
 		   break;
 		}
 	     }
 
-	     if(duplicate_user == 0)
+	     if(duplicate_user == 0 && user_List[i].chatroom_idx == 1)
 	     {
 		mvprintw(show_user, 51,"%s, %d", user_List[i].nick, user_List[i].uuid);
 		show_user++;
 	     }
 	  }
-	  //mvprintw(show_user, 51,"%d", user_List.size());
+	  
 	  mvprintw(17,6, "");
 	}
 
@@ -594,13 +793,13 @@ void *show_message(void *ptr)
 	  
 	    for(unsigned int j = 0; j < user_List.size(); j++)
 	    {
-		if(user_List[j].uuid == message_List[i].uuid)
+		if(user_List[j].uuid == message_List[i].uuid && message_List[i].chatroom_idx == 1 && user_List[j].chatroom_idx == 1)
 		{
-		   mvprintw(show_msg, 2,"%s: %s", user_List[j].nick, message_List[i].message);
+		   mvprintw(show_msg1, 2,"%s: %s", user_List[j].nick, message_List[i].message);
 		   mvprintw(17,6, "");
 		}
 	    }
-	    show_msg++;
+	    show_msg1++;
 	  
 	}
 	count_room = room_List.size();
@@ -609,5 +808,99 @@ void *show_message(void *ptr)
   }
 }
 
+void *show_message1(void *ptr)
+{
+  int count_message = 0;
+  int count_user = 0;
+  int count_room = 0;
+  
+  int second_run = 0;
+  int duplicate_user;
 
+  while(1)
+  {
+    	sleep(0.1);
+    	Message.recv ( &message_List );
+    	User.recv (&user_List);
+	chatRoom.recv(&room_List);
+
+
+	if(sig == 0)
+	{
+	   break;
+	}
+
+	//clear out line when chat message is a lot
+	if(show_msg2 == 34)
+	{
+		show_msg2 = 22;
+		int new_show_msg;
+		for(new_show_msg = 22; new_show_msg < 35; new_show_msg++)
+		{
+		  mvprintw(new_show_msg,2, "                           ");
+		}			  
+	    	second_run ++;
+	}
+	
+	if(count_room != room_List.size())
+	{
+	   mvprintw(25, 52, "          ");
+	   for(unsigned int i = room_List.size(); i > 0; i--)
+	   {
+	      if(room_List[i-1].chatroom_idx == 2)
+		{
+		   mvprintw(25, 52, "%s", room_List[i-1].chatroom_name);
+		   break;
+		}
+	   }
+	   mvprintw(36,6, "");
+	}
+
+	if(count_user != user_List.size())
+	{
+	  int show_user = 32;
+	  for(int i = user_List.size()-1; i >= 0 ; i--)
+	  {
+	     //check for changed name
+	     duplicate_user = 0;
+	     for(int j = i + 1; j < user_List.size(); j++ )
+	     {
+		if(user_List[i].uuid == user_List[j].uuid && user_List[i].chatroom_idx == 2 && user_List[j].chatroom_idx == 2)
+		{
+		   duplicate_user = 1;
+		   break;
+		}
+	     }
+
+	     if(duplicate_user == 0 && user_List[i].chatroom_idx == 2)
+	     {
+		mvprintw(show_user, 51,"%s, %d", user_List[i].nick, user_List[i].uuid);
+		show_user++;
+	     }
+	  }
+	  
+	  mvprintw(36,6, "");
+	}
+
+	//use to check if there is new message in the database
+	if(count_message != message_List.size())
+	{
+	  unsigned int i= message_List.size() - 1;
+	  
+	    for(unsigned int j = 0; j < user_List.size(); j++)
+	    {
+		if(user_List[j].uuid == message_List[i].uuid && message_List[i].chatroom_idx == 2 && user_List[j].chatroom_idx == 2)
+		{
+		   mvprintw(show_msg2, 2,"%s: %s", user_List[j].nick, message_List[i].message);
+		   mvprintw(36,6, "");
+		}
+	    }
+	    show_msg2++;
+	  
+	}
+	count_room = room_List.size();
+	count_message = message_List.size();
+	count_user = user_List.size();
+  }
+}
 
